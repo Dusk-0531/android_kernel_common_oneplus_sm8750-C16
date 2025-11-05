6b864aed24c4d48d6891896446edf3f6ba96ef3f
whitewhale0612 <2136512929@qq.com>
2025-11-05 22:51:10 +0800

    drivers/base：添加get_property_from_cmdline以解析内核 cmdline 引入提取属性get_property_from_cmdline的新函数 内核命令行中的值。此函数接受一个输入字符串 搜索格式为“<input>=”的匹配属性，并返回 相应的值。该实现包括 属性前缀和值，并具有适当的错误处理和日志记录。
    添加新文件 drivers/base/get_prop_from_cmdline.c 及其标头
    include/linux/get_prop_from_cmdline.h。将 drivers/base/Makefile 更新为
    包括新的 boot_mode.o 对象。

--- drivers/of/overwriter/overwrite_config_loader.c
+++ drivers/of/overwriter/overwrite_config_loader.c
@@ -6,6 +6,8 @@
 #include <linux/slab.h>
 #include <linux/moduleparam.h>
 #include <linux/ctype.h>
+#include <linux/get_prop_from_cmdline.h>
+
 #include "../of_private.h"
 
 #include "overwrite_configs.h"
@@ -961,50 +963,10 @@ static int __init patch_device_tree(const char *input)
     return 0;
 }
 
-static char *get_property_from_cmdline(const char *input)
-{
-    char *cmdline = saved_command_line;
-    char *prop_buf = NULL;
-    char *prop_start, *prop_end;
-    int len;
-
-    pr_info("Kernel cmdline: %s\n", cmdline);
-
-    /* Construct the property prefix (e.g., "input=") */
-    char *prop_prefix = kmalloc(strlen(input) + 2, GFP_ATOMIC);
-    if (!prop_prefix) {
-        pr_err("Failed to allocate memory for property prefix\n");
-        return NULL;
-    }
-    snprintf(prop_prefix, strlen(input) + 2, "%s=", input);
-
-    /* Find the property in the command line */
-    prop_start = strstr(cmdline, prop_prefix);
-    if (prop_start) {
-        prop_start += strlen(prop_prefix);
-        prop_end = strchr(prop_start, ' ');
-        len = prop_end ? (prop_end - prop_start) : strlen(prop_start);
-
-        /* Allocate memory for the property value */
-        prop_buf = kmalloc(len + 1, GFP_ATOMIC);
-        if (prop_buf) {
-            strncpy(prop_buf, prop_start, len);
-            prop_buf[len] = '\0';
-            pr_info("Found property %s: %s\n", input, prop_buf);
-        } else {
-            pr_err("Failed to allocate memory for property value\n");
-        }
-    } else {
-        pr_err("Property %s not found in cmdline\n", input);
-    }
-
-    kfree(prop_prefix);
-    return prop_buf ? prop_buf : NULL;
-}
-
 static int __init overwrite_config_init(void)
 {
     char *device_name = get_property_from_cmdline("oplusboot.prjname");
+    char *enable = get_property_from_cmdline("overwrite.enable");
     
     const struct overwrite_config_group *common_group = NULL;
     const struct overwrite_config_group *device_group = NULL;
@@ -1031,6 +993,11 @@ static int __init overwrite_config_init(void)
         pr_info("No common configs found.\n");
     }
 
+    if (enable && strcmp(enable, "0") == 0) {
+        pr_info("Overwrite configs disabled.\n");
+        goto out;
+    }
+
     // 应用设备特有配置（如果存在）
     if (device_group) {
         pr_info("Applying device-specific configs for %s...\n", device_name);
@@ -1041,7 +1008,8 @@ static int __init overwrite_config_init(void)
     } else {
         pr_info("No device-specific configs found for %s.\n", device_name ? device_name : "none");
     }
-    
+
+out:
     kfree(device_name); // 释放获取到的设备名内存
     return 0;
 }
