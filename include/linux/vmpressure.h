a91b861135f0f6d14624a3c02a3eaef8e0a17c57
whitewhale0612 <2136512929@qq.com>
2025-11-06 00:03:42 +0800

    fix conflict types

--- include/linux/vmpressure.h
+++ include/linux/vmpressure.h
@@ -28,12 +28,12 @@ struct vmpressure {
 };
 
 struct mem_cgroup;
-extern void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio,
-			    int order);
+
 #ifdef CONFIG_MEMCG
 extern void vmpressure(gfp_t gfp, struct mem_cgroup *memcg, bool tree,
 		       unsigned long scanned, unsigned long reclaimed);
-extern void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio);
+extern void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio,
+			    int order);
 
 extern void vmpressure_init(struct vmpressure *vmpr);
 extern void vmpressure_cleanup(struct vmpressure *vmpr);
