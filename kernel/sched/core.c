9745bc6b45e4eb7fe9ecde9902d41e06ad977a75
whitewhale0612 <2136512929@qq.com>
2025-11-11 18:49:31 +0800

    Revert "修复上一个提交带来的编译错误"
    This reverts commit 4250df6e9b3f97f82b61683e0e19cbeb674c8b41.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7759,8 +7759,13 @@ unsigned long effective_cpu_util(int cpu, unsigned long util_cfs,
 {
 	unsigned long util, irq, scale;
 	struct rq *rq = cpu_rq(cpu);
+	unsigned long new_util = ULONG_MAX;
 
 	scale = arch_scale_cpu_capacity(cpu);
+
+	trace_android_rvh_effective_cpu_util(cpu, util_cfs, max, type, p, &new_util);
+	if (new_util != ULONG_MAX)
+		return new_util;
 
 	/*
 	 * Early check to see if IRQ/steal time saturates the CPU, can be
