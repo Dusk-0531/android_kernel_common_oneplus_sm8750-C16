2b5933d179b112ccbad9367ccb702e21450607fb
whitewhale0612 <2136512929@qq.com>
2025-11-11 18:49:44 +0800

    Revert "sched/fair: Fix update_cfs_group() vs DELAY_DEQUEUE"
    This reverts commit 8a418eed8f8b5c624dc9661dea151a9da0b5fea1.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4030,11 +4030,7 @@ static void update_cfs_group(struct sched_entity *se)
 	struct cfs_rq *gcfs_rq = group_cfs_rq(se);
 	long shares;
 
-	/*
-	 * When a group becomes empty, preserve its weight. This matters for
-	 * DELAY_DEQUEUE.
-	 */
-	if (!gcfs_rq || !gcfs_rq->load.weight)
+	if (!gcfs_rq)
 		return;
 
 	if (throttled_hierarchy(gcfs_rq))
